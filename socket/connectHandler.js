d1b06ef72e8cea063b36486c90e5fb97c54c8c50
kongtaemin <rhdxoals@gmail.com>
2022-05-11 01:34:22 +0900

    채팅방 참가,나가기, 비디오처리 완료

--- socket/connectHandler.js
+++ socket/connectHandler.js
@@ -1,42 +1,59 @@
-
-let roomList = {
-
-}
+let onlineList =[];
+let roomCnt = 3;
+let roomList = [
+    {id:1,roomName:'전체 채팅방',userList:[]},
+    {id:2,roomName:'전체 채팅방2',userList:[]},
+];
 
 function socketHandler(io){
-
     io.on('connect',(socket) =>{
         // 연결시
-        console.log('a user connected');
-        socket.on('join room',({roomName, peerId, userId,socketId})=>{
-            if(!roomList[roomName]){
-                roomList[roomName] = [{peerId,userId,socketId}];
-            }else{
-                roomList[roomName].push({peerId,userId,socketId});
-            }
+
+        socket.on('login',(userId)=>{
+            onlineList.push({userId,socketId:socket.id});
+            io.emit('online list',onlineList);
             io.emit('room list',roomList);
+
+            console.log('roomList',roomList);
+            console.log('onlineList',onlineList)
         })
+
+        socket.on('join room',({roomId, peerId, userId, socketId})=>{
+            roomList.find(r=> r.id === roomId).userList.push({userId,peerId,socketId,startTime:new Date()});
+            io.emit('room list',roomList);
+            console.log('room list',roomList);
+        })
+
+        socket.on('leave room', ({roomId,socketId})=>{
+            handleLeaveUserFromRoomList(roomId,socketId);
+            io.emit('room list',roomList);
+            console.log(roomList);
+        })
+
         // 연결 해제시
         socket.on('disconnect', () => {
-            handleLeaveUserFromRoomList(socket.id);
+            handleLeaveUserFromAllRooms(socket.id);
+            makeOffline(socket.id);
             io.emit('room list',roomList);
+            io.emit('online list',onlineList);
+            console.log('roomList',roomList);
+            console.log('onlineList',onlineList)
         });
-
     });
 }
 
-function handleLeaveUserFromRoomList(socketId){
-    let newRoomList = {};
-    Object.keys(roomList).forEach(room=>{
-        let userList = []
-        roomList[room].forEach(user=>{
-            if(user.socketId !== socketId){
-                userList.push(user);
-            }
-        })
-        newRoomList[room] = userList;
+function handleLeaveUserFromRoomList(roomId,socketId){
+    const updateduserList = roomList.find(room=> room.id === roomId).userList.filter(u => u.socketId !== socketId);
+    roomList.find(room=> room.id === roomId).userList = updateduserList;
+}
+function handleLeaveUserFromAllRooms(socketId){
+    roomList.forEach(room=>{
+        room.userList = room.userList.filter(u=> u.socketId !== socketId);
     })
-    roomList = newRoomList;
+}
+
+function makeOffline(socketId){
+    onlineList = onlineList.filter(user=> user.socketId !== socketId);
 }
 
 
