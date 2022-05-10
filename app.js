9ba3940492c9a3d7ecbe80895afa4c07cc12a8ef
kongtaemin <rhdxoals@gmail.com>
2022-05-10 16:55:26 +0900

    서버에서 roomList 현황 유지 완료 (유저가 들어오면추가, 나가면 삭제, 최신 roomList 모든유저에게 전송)

--- app.js
+++ app.js
@@ -6,15 +6,18 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const ejs = require('ejs');
 const connectHandler = require("./socket/connectHandler");
+const socketHandler = require("./socket/connectHandler");
 
 
 app.set('view engine','ejs');
 app.set('views','view');
 
-io.on('connection', connectHandler);
 
 app.get('/',(req,res)=>{
     res.render('index');
 })
 
 server.listen(3000);
+
+socketHandler(io);
+
--- socket/connectHandler.js
+++ socket/connectHandler.js
@@ -1,9 +1,43 @@
-module.exports = function connectHandler (socket) {
-    // 연결시
-    console.log('a user connected');
 
-    // 연결 해제시
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+let roomList = {
+
+}
+
+function socketHandler(io){
+
+    io.on('connect',(socket) =>{
+        // 연결시
+        console.log('a user connected');
+        socket.on('join room',({roomName, peerId, userId,socketId})=>{
+            if(!roomList[roomName]){
+                roomList[roomName] = [{peerId,userId,socketId}];
+            }else{
+                roomList[roomName].push({peerId,userId,socketId});
+            }
+            io.emit('room list',roomList);
+        })
+        // 연결 해제시
+        socket.on('disconnect', () => {
+            handleLeaveUserFromRoomList(socket.id);
+            io.emit('room list',roomList);
+        });
+
     });
 }
+
+function handleLeaveUserFromRoomList(socketId){
+    let newRoomList = {};
+    Object.keys(roomList).forEach(room=>{
+        let userList = []
+        roomList[room].forEach(user=>{
+            if(user.socketId !== socketId){
+                userList.push(user);
+            }
+        })
+        newRoomList[room] = userList;
+    })
+    roomList = newRoomList;
+}
+
+
+module.exports = socketHandler;
